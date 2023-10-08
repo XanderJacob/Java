2353844b1f4cb7db8f303e6f858f58ea8f78428e
TheXander <143763742+XanderJacob@users.noreply.github.com>
2023-10-09 00:15:36 +0800

    Add files via upload

---
+++ EmpProject6/src/main/java/com/mycompany/empproject6/EmployeeRoster.java
@@ -0,0 +1,119 @@
+package com.mycompany.empproject6;
+
+import java.util.ArrayList;
+
+public class EmployeeRoster {
+
+    private ArrayList<Employee> empList;
+    private int size;
+
+    public EmployeeRoster() {
+        empList = new ArrayList<>();
+    }
+
+    public EmployeeRoster(ArrayList<Employee> empList) {
+        this.empList = new ArrayList<>();
+        this.empList = empList;
+        this.size = empList.size();
+    }
+
+    public EmployeeRoster(ArrayList<Employee> empList, int size) {
+        this.empList = empList;
+        this.size = size;
+        this.empList = new ArrayList<>();
+    }
+
+    public ArrayList<Employee> getEmpList() {
+        return empList;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setEmpList(ArrayList<Employee> empList) {
+        this.empList = empList;
+        this.size = empList.size();
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public void displayAllEmployee() {
+        for (Employee employee : empList) {
+            if (employee != null) {
+                employee.displayInfo();
+                System.out.println("");
+            }
+        }
+    }
+
+    public boolean addEmployee(Employee e) {
+        if (empList.size() < size) {
+            empList.add(e);
+            System.out.println("Employee " + e.getName().getLastName() + " with ID #" + e.getEmpID());
+            return true;
+        } else {
+            System.out.println("Roster is Full, Please Apply to another Company");
+            return false;
+        }
+    }
+
+    public Employee removeEmployee(int id) {
+        for (Employee employee : empList) {
+            if (employee != null && id == employee.getEmpID()) {
+                empList.remove(employee);
+                return employee;
+            }
+        }
+        System.out.println("Employee " + id + " not Found");
+        return null;
+    }
+
+    public void displayEmployeeByType(String type) {
+        String displayMessage = "";
+
+        switch (type.toUpperCase().charAt(0)) {
+            case 'H' -> displayMessage = "Displaying Hourly Employees: \n";
+            case 'C' -> displayMessage = "Displaying Commission Employees: \n";
+            case 'B' -> displayMessage = "Displaying Base Plus Commission Employees: \n";
+            case 'P' -> displayMessage = "Displaying Piece Worker Employees: \n";
+            default -> {
+                System.out.println("Unknown employee type: " + type);
+                return;
+            }
+        }
+        System.out.println(displayMessage);
+        for (Employee employee : empList) {
+            if (employee != null) {
+                switch (type.toUpperCase().charAt(0)) {
+                    case 'H' -> {
+                        if (employee instanceof HourlyEmployee) {
+                            employee.displayInfo();
+                            System.out.println("");
+                        }
+                    }
+                    case 'C' -> {
+                        if (employee instanceof CommissionEmployee) {
+                            employee.displayInfo();
+                            System.out.println("");
+                        }
+                    }
+                    case 'B' -> {
+                        if (employee instanceof BasePlusCommissionEmployee) {
+                            employee.displayInfo();
+                            System.out.println("");
+                        }
+                    }
+                    case 'P' -> {
+                        if (employee instanceof PieceWorkerEmployee) {
+                            employee.displayInfo();
+                            System.out.println("");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ EmpProject6/src/main/java/com/mycompany/empproject6/TestMain.java
@@ -0,0 +1,40 @@
+package com.mycompany.empproject6;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+public class TestMain {
+
+    public static void main(String[] args) {
+     
+        HourlyEmployee emp1 = new HourlyEmployee(1, new Name("Xander", "Labide"), LocalDate.now(), LocalDate.of(2003, 10, 7), 45, 10.0f);
+        PieceWorkerEmployee emp2 = new PieceWorkerEmployee(2, new Name("Mikee", "Dionson"), LocalDate.now(), LocalDate.of(2003, 9, 10), 10, 25.5f);
+        CommissionEmployee emp3 = new CommissionEmployee(3, new Name("Steph", "Curry"), LocalDate.of(2020, 10, 30), LocalDate.of(2001, 1, 5), 10, 90);
+        BasePlusCommissionEmployee emp4 = new BasePlusCommissionEmployee(4, new Name("Juan", "Dela Cruz"), LocalDate.of(2020, 10, 30), LocalDate.of(1999, 7, 1), 10, 90);
+
+        ArrayList<Employee> roster = new ArrayList<>();
+
+        roster.add(emp1);
+        roster.add(emp2);
+        roster.add(emp3);
+        roster.add(emp4);
+
+        System.out.println("All Employees:");
+        for (Employee employee : roster) {
+            employee.displayInfo();
+            System.out.println("");
+        }
+
+        EmployeeRoster employeeRoster = new EmployeeRoster(roster);
+
+        HourlyEmployee emp5 = new HourlyEmployee(5, new Name("John", "Doe"), LocalDate.now(), LocalDate.of(2000, 5, 15), 40, 15.0f);
+        employeeRoster.addEmployee(emp5);
+
+        
+ 
+        employeeRoster.displayEmployeeByType("H");
+        employeeRoster.displayEmployeeByType("C");
+        employeeRoster.displayEmployeeByType("B");
+        employeeRoster.displayEmployeeByType("P");
+    }
+}
